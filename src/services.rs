393e7df2a7bd3b1f0b5107ac6d67b896f81ef9c5
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-10-28 03:12:12 -0400

    feat: add service to get project

---
+++ src/services.rs
@@ -0,0 +1,31 @@
+use once_cell::sync::Lazy;
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::{Client, ClientBuilder};
+
+use crate::error::MResult;
+use crate::MODRINTH_API_KEY;
+
+pub mod projects;
+
+pub static API_BASE_URL: Lazy<String> = Lazy::new(|| lc!(macro_env!(File, "API_BASE_URL")));
+const APP_USER_AGENT: &str = concat!(
+    "SergioRibera",
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+);
+
+pub fn create_client() -> MResult<Client> {
+    let mut headers = HeaderMap::new();
+    headers
+        .insert(
+            "Authorization",
+            HeaderValue::from_str(MODRINTH_API_KEY.as_str())?,
+        )
+        .unwrap();
+
+    Ok(ClientBuilder::new()
+        .user_agent(APP_USER_AGENT)
+        .default_headers(headers)
+        .build()?)
+}
---
+++ src/services/projects.rs
@@ -0,0 +1,22 @@
+use reqwest::Client;
+
+use crate::error::MResult;
+use crate::models::project::Project;
+use crate::services::API_BASE_URL;
+
+const BASE_PATH: &str = "/project/";
+
+fn build_url(extra: String) -> String {
+    format!("{}{BASE_PATH}{extra}", API_BASE_URL.as_str())
+}
+
+/// The main idea with separating this by modules is that in each module
+/// of this folder would go the service corresponding to the consumption of each api,
+/// the ideal would be to implement a structure with a trait,
+/// but for the size of this project and the requirement to be a single request,
+/// it is not worth implementing the trait.
+pub async fn get(client: &Client, slug: String) -> MResult<Project> {
+    let url = build_url(slug);
+    let res = client.get(url).send().await?.json::<Project>().await?;
+    Ok(res)
+}
